45d7b5c72e9838e543cfe1d0ee8671fb125b14b8
Flávio Ricardo <flavioricardo@live.com>
2019-04-03 15:07:24 -0300

    Criando componente Escolas e Mapa

--- src/components/Escolas/Escolas.js
+++ src/components/Escolas/Escolas.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Mapa from '../Mapa/Mapa';
 
 class Escolas extends Component {
 
     render() {
         return(
-            <div></div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                    </div>
+                    <Mapa />
+                </div>
+            </div>
         );
     }
 
---
+++ src/components/Mapa/Mapa.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Mapa extends Component {
+
+    render() {
+        return(
+            <div></div>
+        );
+    }
+
+}
+
+export default Mapa;
