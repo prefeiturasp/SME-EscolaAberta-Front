8e671c7c7f0d475b29dd08e721d03873da9f20ff
Flávio Ricardo <flavioricardo@live.com>
2019-04-05 18:04:11 -0300

    Consumindo a API de escolas

--- src/components/Escolas/Escolas.js
+++ src/components/Escolas/Escolas.js
@@ -3,11 +3,51 @@ import Mapa from '../Mapa/Mapa';
 
 class Escolas extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            escolas : []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://hom-escolaaberta.sme.prefeitura.sp.gov.br/api/escolas/').then(resultados => {
+            resultados.json().then(escolas => {
+                this.setState({ escolas : escolas.results })
+            })
+        })
+        // console.log(this.state.escolas)
+    }
+
     render() {
         return(
             <div className="container">
                 <div className="row">
                     <div className="col-6">
+                    <table className="table-bordered table-striped">
+                        <thead className="thead-light">
+                            <th>Código</th>
+                            <th>Nome da escola</th>
+                            <th>Tipo</th>
+                            <th>DRE</th>
+                            <th></th>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.escolas.map(function(escola, indice) {
+                                    return (
+                                        <tr key={ indice }>
+                                            <td>{ escola.codesc }</td>
+                                            <td>{ escola.nomesc }</td>
+                                            <td>{ escola.tipoesc }</td>
+                                            <td>{ escola.subpref }</td>
+                                            <td><input type="checkbox" /></td>
+                                        </tr>
+                                    );
+                                })
+                            }
+                        </tbody>
+                    </table>
                     </div>
                     <Mapa />
                 </div>
--- src/components/MenuSuperior/MenuSuperior.js
+++ src/components/MenuSuperior/MenuSuperior.js
@@ -4,7 +4,35 @@ class MenuSuperior extends Component {
 
     render() {
         return(
-            <div></div>
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <h1>Escola Aberta</h1>
+                        </div>
+                        <div className="col-6">
+                            <h2>Consulte sua posição</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="menu-busca p-3">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-5">
+                                <select name="escola" id="escola" className="custom-select rounded-pill">
+                                    <option value="">Selecione a escola</option>
+                                </select>
+                            </div>
+                            <div className="col-3">
+                                <input type="text" className="form-control rounded-pill" />
+                            </div>
+                            <div className="col-4">
+                            <   input type="text" className="form-control rounded-pill" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 
