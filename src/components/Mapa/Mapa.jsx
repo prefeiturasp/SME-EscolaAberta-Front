ce9dca17e4502c27363a856f1e60f8c980452e53
Flávio Ricardo <flavioricardo@live.com>
2019-04-29 19:01:41 -0300

    Salvando estado da area selecionada do mapa

--- src/components/Mapa/Mapa.jsx
+++ src/components/Mapa/Mapa.jsx
@@ -57,7 +57,7 @@ export default class Mapa extends Component {
     }
 
     buscarEscolasArea() {
-        console.log(this.refs.map.leafletElement.getBounds());
+        this.setState({ area : this.refs.map.leafletElement.getBounds() });
     }
 
     render() {
