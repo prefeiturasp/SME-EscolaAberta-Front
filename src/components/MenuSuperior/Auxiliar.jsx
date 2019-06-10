8ccde91f901610d5c3f2368de0f03d9ec34be677
Flávio Ricardo <flavioricardo@live.com>
2019-06-10 11:20:42 -0300

    Consulta de Posição Consolidada

---
+++ src/components/MenuSuperior/Auxiliar.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSlidersH } from "@fortawesome/free-solid-svg-icons";
+
+export default class Auxiliar extends Component {
+  render() {
+    return (
+      <div className="w-100 menu-auxiliar h-100 p-3 text-white">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-sm-12 d-flex justify-content-start align-items-center">
+              <h3 className="font-weight-bold m-0">{this.props.texto}</h3>
+            </div>
+            {this.props.filtro ? (
+              <div className="col-lg-6 col-sm-12 d-flex justify-content-lg-end justify-content-start align-items-center">
+                <button className="btn btn-lg btn-outline-light" data-toggle="collapse" data-target="#filtro-collapse" aria-expanded="false" aria-controls="filtro-collapse">
+                  <FontAwesomeIcon icon={faSlidersH} /> Filtrar
+              </button>
+              </div>
+            ) : (null)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
