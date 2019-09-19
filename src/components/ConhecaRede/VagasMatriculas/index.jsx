b5c44d67db93f0277cc79d8e261685eb7a37a1a0
Calvin Rossinhole <calvin.masters@gmail.com>
2019-09-19 17:52:20 -0300

    conheca a rede - vagas e matriculas - filtro por dre; filtro por serie; mescla de EF 9 anos e EF de 9 anos

--- src/components/ConhecaRede/VagasMatriculas/index.jsx
+++ src/components/ConhecaRede/VagasMatriculas/index.jsx
@@ -8,7 +8,6 @@ import { faIdCard } from "@fortawesome/free-solid-svg-icons";
 import { ToggleExpandir } from "../../ToggleExpandir";
 import { formatarVagasMatriculas } from "./helper";
 import { getKey } from "../helper";
-import { DoisNiveisChart } from "../../Graficos/DoisNiveisChart";
 import "./style.scss";
 
 export class VagasMatriculas extends Component {
@@ -16,8 +15,20 @@ export class VagasMatriculas extends Component {
     super(props);
     this.state = {
       vagasMatriculas: [],
+      checks: [
+        "EDUCACAO INFANTIL",
+        "ENSINO FUNDAMENTAL 9 ANOS",
+        "ENSINO MEDIO",
+        "ENSINO MEDIO NORMAL/MAGISTERIO",
+        "ESPEC ENS MEDIO",
+        "TECNICO MEDIO",
+        "EJA CIEJA",
+        "EJA ESCOLAS EDUCACAO ESPECIAL",
+        "EJA ESCOLAS ENSINO FUNDAMENTAL"
+      ],
       referencia: "",
-      ativo: false
+      ativo: false,
+      primeiroCheck: false
     };
   }
 
@@ -36,7 +47,9 @@ export class VagasMatriculas extends Component {
 
   onSelectChanged(value) {
     listarVagasMatriculasSMEPorDRE({ dre: value }).then(lista => {
-      this.setState({ ambientes: lista.results });
+      this.setState({
+        vagasMatriculas: formatarVagasMatriculas(lista.results)
+      });
     });
   }
 
@@ -52,9 +65,45 @@ export class VagasMatriculas extends Component {
     this.setState({ vagasMatriculas, ativo: !this.state.ativo });
   }
 
+  onCheckClicked(labels) {
+    let checks = this.state.checks;
+    if (!this.state.primeiroCheck) {
+      checks = [];
+      this.setState({ primeiroCheck: true });
+    }
+    if (!checks.includes(labels[0])) {
+      labels.forEach(label => {
+        checks.push(label);
+      });
+    } else {
+      labels.forEach(label => {
+        checks.forEach((check, index) => {
+          if (check === label) {
+            checks.splice(index, 1);
+          }
+        });
+      });
+      if (checks.length === 0) {
+        checks = [
+          "EDUCACAO INFANTIL",
+          "ENSINO FUNDAMENTAL 9 ANOS",
+          "ENSINO MEDIO",
+          "ENSINO MEDIO NORMAL/MAGISTERIO",
+          "ESPEC ENS MEDIO",
+          "TECNICO MEDIO",
+          "EJA CIEJA",
+          "EJA ESCOLAS EDUCACAO ESPECIAL",
+          "EJA ESCOLAS ENSINO FUNDAMENTAL"
+        ];
+        this.setState({ primeiroCheck: false })
+      }
+    }
+    this.setState({ checks });
+  }
+
   render() {
     const { diretoriasRegionais } = this.props;
-    const { referencia, ativo, vagasMatriculas } = this.state;
+    const { referencia, ativo, vagasMatriculas, checks } = this.state;
     return (
       <div className="mt-5 mb-5">
         <div className="estatisticas-cabecalho mb-5">
@@ -88,23 +137,48 @@ export class VagasMatriculas extends Component {
             <div className="ml-3 fonte-14">Vagas e Matrículas</div>
             <div className="checkboxes ml-auto">
               <span>
-                <input type="checkbox" />
+                <input
+                  onClick={() => this.onCheckClicked(["EDUCACAO INFANTIL"])}
+                  type="checkbox"
+                />
                 Infantil
               </span>
               <span>
-                <input type="checkbox" />
-                Fundamental I
+                <input
+                  onClick={() =>
+                    this.onCheckClicked([
+                      "ENSINO FUNDAMENTAL 9 ANOS"
+                    ])
+                  }
+                  type="checkbox"
+                />
+                Fundamental
               </span>
               <span>
-                <input type="checkbox" />
-                Fundamental II
-              </span>
-              <span>
-                <input type="checkbox" />
+                <input
+                  onClick={() =>
+                    this.onCheckClicked([
+                      "ENSINO MEDIO",
+                      "ENSINO MEDIO NORMAL/MAGISTERIO",
+                      "ESPEC ENS MEDIO",
+                      "TECNICO MEDIO"
+                    ])
+                  }
+                  type="checkbox"
+                />
                 Médio
               </span>
               <span>
-                <input type="checkbox" />
+                <input
+                  onClick={() =>
+                    this.onCheckClicked([
+                      "EJA CIEJA",
+                      "EJA ESCOLAS EDUCACAO ESPECIAL",
+                      "EJA ESCOLAS ENSINO FUNDAMENTAL"
+                    ])
+                  }
+                  type="checkbox"
+                />
                 EJA
               </span>
             </div>
@@ -127,23 +201,27 @@ export class VagasMatriculas extends Component {
                 <tbody>
                   {vagasMatriculas.map((matricula, indice) => {
                     return [
-                      <tr className="main" key={indice}>
-                        <td className="font-weight-bold">
-                          {getKey(matricula)}
-                        </td>
-                        <td>{matricula[getKey(matricula)].total_turmas}</td>
-                        <td>{matricula[getKey(matricula)].vagas_oferecidas}</td>
-                        <td>
-                          {matricula[getKey(matricula)].vagas_remanecentes}
-                        </td>
-                        <td>
-                          {matricula[getKey(matricula)].media_atendimento}{" "}
-                          <ToggleExpandir
-                            ativo={matricula[getKey(matricula)].ativo}
-                            onClick={() => this.onMatriculaClicked(matricula)}
-                          />
-                        </td>
-                      </tr>,
+                      checks.includes(getKey(matricula)) && (
+                        <tr className="main" key={indice}>
+                          <td className="font-weight-bold">
+                            {getKey(matricula)}
+                          </td>
+                          <td>{matricula[getKey(matricula)].total_turmas}</td>
+                          <td>
+                            {matricula[getKey(matricula)].vagas_oferecidas}
+                          </td>
+                          <td>
+                            {matricula[getKey(matricula)].vagas_remanecentes}
+                          </td>
+                          <td>
+                            {matricula[getKey(matricula)].media_atendimento}{" "}
+                            <ToggleExpandir
+                              ativo={matricula[getKey(matricula)].ativo}
+                              onClick={() => this.onMatriculaClicked(matricula)}
+                            />
+                          </td>
+                        </tr>
+                      ),
                       matricula[getKey(matricula)].ativo &&
                         matricula[getKey(matricula)].decseries.map(
                           (decserie, indice_) => {
@@ -165,9 +243,6 @@ export class VagasMatriculas extends Component {
                 </tbody>
               </table>
             </div>
-          </div>
-          <div>
-            <DoisNiveisChart />
           </div>
         </div>
       </div>
