9b106d70b97c2ad02cd482bc6b2ea6c69c86ca66
Calvin Rossinhole <calvin.masters@gmail.com>
2019-09-29 21:39:32 -0300

    adiciona href para logo sme

--- src/components/MenuSuperior/Menu.jsx
+++ src/components/MenuSuperior/Menu.jsx
@@ -151,13 +151,13 @@ export default class Menu extends Component {
           <div className="row mt-4 mb-4">
             <div className="col-lg-3 col-sm-12 d-flex justify-content-lg-start justify-content-center align-items-end mb-4 mb-lg-0">
               <h1 className="m-0">
-                <Link to="/">
+                <a href="http://portal.sme.prefeitura.sp.gov.br/">
                   <img
                     src={logoEducacaoSP}
                     alt="Escola Aberta"
                     className="img-fluid"
                   />
-                </Link>
+                </a>
               </h1>
             </div>
             <div
