5140ae7450ef14edb8fcb0e1cd27812323523e9d
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-09-30 16:16:39 -0300

    Card 5131 - Ajustes no ambiente de homologação - Corrigir retorno da busca por nome da escola

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -79,11 +79,17 @@ export default class Buscador extends Component {
         );
       }
     }
-  }
+  };
+
+  removerAcentos = (string_com_acentuacao) => {
+    return string_com_acentuacao.normalize('NFD').replace(/[\u0300-\u036f]/g, '') // Remove acentos
+  };
 
   buscarEscolasPorNome(e) {
     let escolas = [];
-    listarEscolas({ escola: e }).then(lista => {
+    const string_sem_acento = this.removerAcentos(e);
+
+    listarEscolas({ escola: string_sem_acento }).then(lista => {
       lista.results.forEach(function (escola) {
         escolas.push({ value: escola.codesc, label: escola.nomesc });
       });
