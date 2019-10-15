4611cd1f611769a8bcc662b8e4754637061d30aa
Calvin Rossinhole <calvin.masters@gmail.com>
2019-10-15 18:14:13 -0300

    corrige dados vazios idep

--- src/components/Estatisticas/Idep/helper.js
+++ src/components/Estatisticas/Idep/helper.js
@@ -1,21 +1,25 @@
 export const formatarDados = dados => {
-  let indices_indices = [];
-  dados.indices.indices.forEach(indice => {
-    try {
-      indices_indices.push(indice.toFixed(2));
-    } catch {
-      indices_indices.push(indice);
-    }
-  });
-  dados.indices.indices = indices_indices;
-  let metas_metas = [];
-  dados.metas.metas.forEach(meta => {
-    try {
-      metas_metas.push(meta.toFixed(2));
-    } catch {
-      metas_metas.push(meta);
-    }
-  });
-  dados.metas.metas = metas_metas;
+  if (typeof dados.indices.indices === "object") {
+    let indices_indices = [];
+    dados.indices.indices.forEach(indice => {
+      try {
+        indices_indices.push(indice.toFixed(2));
+      } catch {
+        indices_indices.push(indice);
+      }
+    });
+    dados.indices.indices = indices_indices;
+  }
+  if (typeof dados.metas.metas === "object") {
+    let metas_metas = [];
+    dados.metas.metas.forEach(meta => {
+      try {
+        metas_metas.push(meta.toFixed(2));
+      } catch {
+        metas_metas.push(meta);
+      }
+    });
+    dados.metas.metas = metas_metas;
+  }
   return dados;
 };
