215f080b9616c09fdffacb0ad58ff486f326b407
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2021-02-17 14:33:45 -0300

    Incluindo PTRF no Escola Aberta

--- src/components/Estatisticas/Estatisticas.jsx
+++ src/components/Estatisticas/Estatisticas.jsx
@@ -1,8 +1,9 @@
-import React, { Component, lazy, Suspense } from "react";
+import React, {Component, lazy, Suspense} from "react";
 import Menu from "../MenuSuperior/Menu";
 import Auxiliar from "../MenuSuperior/Auxiliar";
 import { Rodape } from "../Rodape";
 import NullView from "./NullView";
+import {DadosPtrf2020} from "./Ptrf/DadosPtrf2020";
 
 import {
   dataReferencia,
@@ -17,6 +18,7 @@ const Profissionais = lazy(() => import("./Profissionais"));
 const VagasMatriculas = lazy(() => import("./VagasMatriculas"));
 const Ambientes = lazy(() => import("./Ambientes"));
 const Idep = lazy(() => import("./Idep/Idep"));
+const Ptrf = lazy(() => import("./Ptrf/"));
 
 export default class Estatisticas extends Component {
   constructor(props) {
@@ -42,8 +44,9 @@ export default class Estatisticas extends Component {
         {
           nome: "Idep",
           label: "IDEP"
-        }
+        },
       ],
+      dadosDaEscolaPtrf: {},
       codesc: "",
       nomesc: "",
       dataReferencia: null,
@@ -55,6 +58,26 @@ export default class Estatisticas extends Component {
     if (this.props.location.state !== undefined) {
       if (this.props.location.state.codesc !== undefined) {
         this.setState({ codesc: this.props.location.state.codesc });
+        let escola = DadosPtrf2020.filter(item => String(item.EOL) === String(this.props.location.state.codesc));
+        if (escola && escola.length > 0){
+          this.setState({
+            dadosDaEscolaPtrf:{
+              EOL: escola[0].EOL,
+              UNIDADE: escola[0].UNIDADE,
+              DRE: escola[0].DRE,
+              BAS_1PAR: escola[0].BAS_1PAR,
+              BAS_2PAR: escola[0].BAS_2PAR,
+              REC_EXTRA: escola[0].REC_EXTRA,
+              BAS_3PAR: escola[0].BAS_3PAR,
+              REC_EXTRA2: escola[0].REC_EXTRA2,
+              EM: escola[0].EM,
+              SALA_LEITURA: escola[0].SALA_LEITURA,
+              MAT_PEDAG: escola[0].MAT_PEDAG,
+              TOTAL: escola[0].TOTAL,
+            }
+          })
+        }
+
       }
       if (this.props.location.state.nomesc !== undefined) {
         this.setState({ nomesc: this.props.location.state.nomesc });
@@ -63,18 +86,15 @@ export default class Estatisticas extends Component {
   }
 
   componentDidMount() {
-    listarServidoresEscolarizacao({ codesc: this.state.codesc }).then(
-      lista1 => {
-        listarServidoresPorEscola({ codesc: this.state.codesc }).then(
-          lista2 => {
-            loginIdep().then(_ => {
-              dadosIdep(this.state.codesc).then(retorno => {
-                if (
-                  lista1.results.length === 0 &&
-                  lista2.length === 0 &&
-                  retorno.result.ano_inicial &&
-                  retorno.result.ano_final
-                ) {
+    listarServidoresEscolarizacao({ codesc: this.state.codesc })
+    .then(lista1 => {
+        listarServidoresPorEscola({ codesc: this.state.codesc })
+        .then(lista2 => {
+            loginIdep()
+            .then(_ => {
+              dadosIdep(this.state.codesc)
+              .then(retorno => {
+                if (lista1.results.length === 0 && lista2.length === 0 && retorno.result.ano_inicial && retorno.result.ano_final) {
                   this.setState({
                     componentesLabels: [
                       {
@@ -92,15 +112,10 @@ export default class Estatisticas extends Component {
                       {
                         nome: "Idep",
                         label: "IDEP"
-                      }
+                      },
                     ]
                   });
-                } else if (
-                  lista1.results.length === 0 &&
-                  lista2.length === 0 &&
-                  !retorno.result.ano_inicial &&
-                  !retorno.result.ano_final
-                ) {
+                } else if (lista1.results.length === 0 && lista2.length === 0 && !retorno.result.ano_inicial && !retorno.result.ano_final) {
                   this.setState({
                     componentesLabels: [
                       {
@@ -117,12 +132,7 @@ export default class Estatisticas extends Component {
                       }
                     ]
                   });
-                } else if (
-                  lista1.results.length > 0 &&
-                  lista2.length > 0 &&
-                  !retorno.result.ano_inicial &&
-                  !retorno.result.ano_final
-                ) {
+                }else if (lista1.results.length > 0 && lista2.length > 0 && !retorno.result.ano_inicial && !retorno.result.ano_final) {
                   this.setState({
                     componentesLabels: [
                       {
@@ -140,9 +150,18 @@ export default class Estatisticas extends Component {
                       {
                         nome: "Ambientes",
                         label: "Ambientes"
-                      }
+                      },
                     ]
                   });
+                }
+                if (this.state.dadosDaEscolaPtrf && this.state.dadosDaEscolaPtrf.EOL){
+                  this.setState(prevState => ({
+                    componentesLabels: [...prevState.componentesLabels,
+                      {
+                        nome: "Ptrf",
+                        label: "PTRF"
+                      }]
+                  }))
                 }
               });
             });
@@ -166,7 +185,7 @@ export default class Estatisticas extends Component {
   }
 
   renderizaComponente(componente) {
-    const { dataReferencia, codesc } = this.state;
+    const { dataReferencia, codesc, dadosDaEscolaPtrf } = this.state;
     switch (componente) {
       case "SeriesEstudantes":
         return (
@@ -184,6 +203,8 @@ export default class Estatisticas extends Component {
         return <Ambientes codesc={codesc} dataReferencia={dataReferencia} />;
       case "Idep":
         return <Idep codesc={codesc} dataReferencia={dataReferencia} />;
+        case "Ptrf":
+        return <Ptrf codesc={codesc} dataReferencia={dataReferencia} dadosDaEscolaPtrf={dadosDaEscolaPtrf} />;
       default:
         return <NullView />;
     }
